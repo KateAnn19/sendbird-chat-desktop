e2bcd0f5a72a9e23e77dad25e030b273a5195c3b
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-01 15:48:07 +0700

    moved router in separated file

--- src/application.jsx
+++ src/application.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import { store } from './redux/store';
+import { Navigator } from './navigation';
 
 import './assets/css/styles.css';
 import './assets/scss/styles.scss';
@@ -13,12 +13,12 @@ import { Banner } from './components/Banner';
 
 export const Application = hot(module)(() => (
   <Provider store={store}>
-    <Router>
+    <Navigator>
       <React.Fragment>
         <h1 className="title">React Starter Template</h1>
         <p>Build with Webpack 4</p>
         <Banner />
       </React.Fragment>
-    </Router>
+    </Navigator>
   </Provider>
 ));
