462dc88e7d9b73f6eb62019241caa08b2a223dc3
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-01 15:55:34 +0700

    added children to navigator component

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-export const Navigator = () => <Router />;
+export const Navigator = ({ children }) => (
+  <Router>
+    {children}
+  </Router>
+);
