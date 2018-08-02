5ec0ccd6471b0d5815657bd2fa6959abda301c8f
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-02 11:38:45 +0700

    added styled-components

--- src/components/AuthorizationForm/index.jsx
+++ src/components/AuthorizationForm/index.jsx
@@ -1,4 +1,33 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Form = styled.form`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  right: 50%;
+  bottom: 50%;
+  transform: translateX(50%) translateY(50%);
+`;
+
+const Legend = styled.legend`
+  font-size: 20px;
+  text-align: center;
+`;
+
+const Input = styled.input.attrs({
+  type: props => props.type,
+  placeholder: props => props.placeholder
+})`
+  margin-bottom: 5px;
+`;
+
+const SubmitButton = styled.input.attrs({
+  type: 'submit',
+  value: props => props.value
+})``;
+
 
 export class AuthorizationForm extends Component {
   constructor() {
@@ -14,13 +43,13 @@ export class AuthorizationForm extends Component {
 
   render() {
     return (
-      <form className="form" action="#">
-        <legend>Войти</legend>
-        <input className="form__item" type="text" placeholder="Логин" />
-        <input className="form__item" type="password" placeholder="Пароль" />
-        <input className="form__item" type="email" placeholder="Email" />
-        <input onClick={this.handleSubmit} type="submit" value="Вход" />
-      </form>
+      <Form>
+        <Legend>Войти</Legend>
+        <Input type="text" placeholder="Логин" />
+        <Input type="password" placeholder="Пароль" />
+        <Input type="email" placeholder="Email" />
+        <SubmitButton onClick={this.handleSubmit} type="submit" value="Вход" />
+      </Form>
     );
   }
 }
