12806ead25984266d5c8521d481e1e754524f1f3
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-02 11:44:53 +0700

    changed to rem in text components

--- src/components/AuthorizationForm/index.jsx
+++ src/components/AuthorizationForm/index.jsx
@@ -5,14 +5,14 @@ const Form = styled.form`
   position: absolute;
   display: flex;
   flex-direction: column;
-  width: 20%;
+  width: 15%;
   right: 50%;
   bottom: 50%;
   transform: translateX(50%) translateY(50%);
 `;
 
 const Legend = styled.legend`
-  font-size: 20px;
+  font-size: 1.3rem;
   text-align: center;
 `;
 
@@ -21,12 +21,15 @@ const Input = styled.input.attrs({
   placeholder: props => props.placeholder
 })`
   margin-bottom: 5px;
+  font-size: 0.8rem;
 `;
 
 const SubmitButton = styled.input.attrs({
   type: 'submit',
   value: props => props.value
-})``;
+})`
+  font-size: 1.2rem;
+`;
 
 
 export class AuthorizationForm extends Component {
