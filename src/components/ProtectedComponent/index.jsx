c0a008530533c8968841c2c921e48c6e0f066103
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-03 13:06:51 +0700

    added redirect, added protected component for testing

---
+++ src/components/ProtectedComponent/index.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const ProtectedComponent = () => <h1>protected zone</h1>;
--- src/navigation.jsx
+++ src/navigation.jsx
@@ -3,15 +3,33 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
 
 import AuthorizationForm from './components/AuthorizationForm';
+import ProtectedComponent from './components/ProtectedComponent';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={
+      props =>
+        false
+          ?
+            (<Component {...props} />)
+          :
+            (<Redirect to={{ pathname: 'login/' }} />)
+        }
+  />
+);
 
 export const Navigator = () => (
   <Router>
     <Switch>
-      <Route path="/" component={AuthorizationForm} />
+      <Route path="/login" component={AuthorizationForm} />
+      <PrivateRoute path="/protected" component={ProtectedComponent} />
     </Switch>
   </Router>
 );
