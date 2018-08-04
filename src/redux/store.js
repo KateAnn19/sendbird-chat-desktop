82efa039f0c7c093b45ada1e25212a3813527950
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-04 19:27:12 +0700

    added logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+
+import { createStore, applyMiddleware, compose } from 'redux';
 import { reducer as userReducer } from './user/reducer';
 import { sagas as userSaga } from './user/sagas';
 
@@ -8,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   userReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  composeEnhancers(applyMiddleware(sagaMiddleware, logger))
 );
 
 sagaMiddleware.run(userSaga);
