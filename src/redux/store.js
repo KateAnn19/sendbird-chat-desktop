cdd62e47a4b0f31f05cc89c58474ab1c189337e0
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-06 12:54:11 +0700

    added connected-react-router

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,21 @@
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { createBrowserHistory } from 'history';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import { reducer as userReducer } from './user/reducer';
 import { sagas as userSaga } from './user/sagas';
 
+export const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  userReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware, logger))
+  connectRouter(history)(userReducer),
+  composeEnhancers(
+    applyMiddleware(routerMiddleware(history), sagaMiddleware, logger)
+  )
 );
 
 sagaMiddleware.run(userSaga);
