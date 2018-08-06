731bf3e3ab221af092eaff64e8273a5082ae1367
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-06 16:50:26 +0700

    fixed typo

--- src/redux/user/requests.js
+++ src/redux/user/requests.js
@@ -4,8 +4,8 @@ export const loginUser = (username, password, email) =>
   api.post('/users/session', { username, password, email });
 export const registerUser = (username, password, email) =>
   api.post('/users', { username, password, email });
-export const checkUserSession = (sbUserId, token) =>
-  api.get(`/users/${sbUserId}/session`, {
+export const checkUserSession = (userId, token) =>
+  api.get(`/users/${userId}/session`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
