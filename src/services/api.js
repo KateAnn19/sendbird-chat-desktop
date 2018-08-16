c695e4cbf189dfe27ed3fbcdb11fadc233ce223b
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-16 08:50:50 +0700

    changed api address

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.88.45:8000',
+  baseURL: 'http://192.168.88.16:8000',
   headers: {
     'content-type': 'application/json',
   },
